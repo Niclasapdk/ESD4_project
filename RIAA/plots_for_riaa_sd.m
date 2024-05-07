f00fdf38504d4b4c3e322fb9dea8a74537dd9a83
JohanASB <jaskja22@student.aau.dk>
2024-05-07 13:28:08 +0200

    fix riaa simulation figs

---
+++ RIAA/plots_for_riaa_sd.m
@@ -0,0 +1,14 @@
+close all; clear;
+[FileA1,PathA1] = uigetfile('*.txt', 'Selec measured frequency reponse with DUT connected');
+FullFileA1 = fullfile(PathA1,FileA1);
+[Freq1 dBfund1 dBfund2 dBfund3] = textread(FullFileA1,'%f %f %f %f','headerlines',1);
+
+s = semilogx(Freq1, dBfund1, Freq1, dBfund2);
+xlabel('Frequency(Hz)','FontSize',12);
+ylabel('dB scale','FontSize',12);
+axis([0 25000, 0 70]);
+s(1).LineWidth = 1.4;
+s(2).LineWidth = 1.4;
+legend({'Line Level Gain Stage','RIAA stage'},'FontSize',16);
+set(gca,'FontSize',16);
+grid on
