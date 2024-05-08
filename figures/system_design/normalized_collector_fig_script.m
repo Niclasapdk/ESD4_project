02e0cf6c15f85d4fa3ea585f5e371d2da2f6d222
JohanASB <jaskja22@student.aau.dk>
2024-05-08 09:27:06 +0200

    fix le fig for nouglas

--- figures/system_design/normalized_collector_fig_script.m
+++ figures/system_design/normalized_collector_fig_script.m
@@ -14,13 +14,12 @@ plot([0 0],get(gca, 'ylim'),'k--','LineWidth',1)
 hold on
 plot(-x, sigmoid, 'linew',3);
 hold on
-plot([0.6 0.6],get(gca, 'ylim'),'k--','LineWidth',1)
+plot([0.8 0.8],get(gca, 'ylim'),'k--','LineWidth',1)
 hold on
-plot([-0.6 -0.6],get(gca, 'ylim'),'k--','LineWidth',1)
+plot([-0.8 -0.8],get(gca, 'ylim'),'k--','LineWidth',1)
 hold on
 plot([-10 0],[0.5 0.5],'--k','LineWidth',1);
 xlabel('Normalized differential input voltage','FontSize',14);
 ylabel('Normalized collector current','FontSize',14);
 set(gca,'FontSize',16);
 hold on
-text(0.7,0.5,'x','FontSize',18);
