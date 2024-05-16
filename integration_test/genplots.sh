4fb76f4b1c93ffc1048e2e1af603aaa4280d48e6
mhtoribio <23717111+mhtoribio@users.noreply.github.com>
2024-05-16 09:39:37 +0200

    reverse order of plot titles
    Example:
    ```
    Power Amplifier THD
    THD: Power Amplifier
    ```

--- integration_test/genplots.sh
+++ integration_test/genplots.sh
@@ -50,12 +50,12 @@ for ((i=0; i<length; i++)); do
     echo "# $title"
     pushd $dir >/dev/null || continue
     echo THD plot
-    python3 $cwd/audio_analyser_plot.py --autodetect --quiet --outfile "thd_${dir//\//_}.png" --title "${title} THD" --type "thd" --logx
+    python3 $cwd/audio_analyser_plot.py --autodetect --quiet --outfile "thd_${dir//\//_}.png" --title "THD: ${title}" --type "thd" --logx
     echo FreqResp plot
-    python3 $cwd/audio_analyser_plot.py --autodetect --quiet --outfile "freqresp_${dir//\//_}.png" --title "${title} Frequency Response" --type "freqresp" --logx $arg
+    python3 $cwd/audio_analyser_plot.py --autodetect --quiet --outfile "freqresp_${dir//\//_}.png" --title "Frequency Response: ${title}" --type "freqresp" --logx $arg
     echo Input Impedance plot
-    python3 $cwd/input_impedance_analogdiscovery.py --autodetect --quiet --outfile "zi_${dir//\//_}.png" --title "${title} Input Impedance" --logx $zi_arg
+    python3 $cwd/input_impedance_analogdiscovery.py --autodetect --quiet --outfile "zi_${dir//\//_}.png" --title "Input Impedance: ${title}" --logx $zi_arg
     echo Output Impedance plot
-    python3 $cwd/output_impedance_analogdiscovery.py --autodetect --quiet --outfile "zo_${dir//\//_}.png" --title "${title} Output Impedance" --logx
+    python3 $cwd/output_impedance_analogdiscovery.py --autodetect --quiet --outfile "zo_${dir//\//_}.png" --title "Output Impedance: ${title}" --logx
     popd >/dev/null
 done
