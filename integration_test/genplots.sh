4bbf5ea397fb3790c62701ccc70a23944eb5e3f7
mhtoribio <23717111+mhtoribio@users.noreply.github.com>
2024-05-16 09:37:34 +0200

    make input impedance kOhm instead of Ohm

--- integration_test/genplots.sh
+++ integration_test/genplots.sh
@@ -2,7 +2,6 @@
 
 cwd=$(pwd)
 dirs=(\
-    "boom_boom"\
     "eq_volctrl/pot_max"\
     "eq_volctrl/pot_min"\
     "eq_volctrl"\
@@ -13,7 +12,6 @@ dirs=(\
     "preamp_riaa_opamp"\
 )
 titles=(\
-    "HiFi Amplifier"\
     "EQ and VC (max)"\
     "EQ and VC (min)"\
     "EQ and VC"\
@@ -26,7 +24,6 @@ titles=(\
 args=(\
     "--simfile simulation_data.txt"\
     "--simfile simulation_data.txt"\
-    "--simfile simulation_data.txt"\
     ""\
     "--simfile simulation_data.txt"\
     "--simfile simulation_data.txt"\
@@ -34,11 +31,22 @@ args=(\
     "--simfile simulation_data.txt --calfile calculation_data.csv"\
     "--simfile simulation_data.txt"\
 )
+zi_args=(\
+    ""\
+    ""\
+    ""\
+    "--logy"\
+    ""\
+    ""\
+    ""\
+    ""\
+)
 length=${#dirs[@]}
 for ((i=0; i<length; i++)); do
     dir=${dirs[i]}
     title=${titles[i]}
     arg=${args[i]}
+    zi_arg=${zi_args[i]}
     echo "# $title"
     pushd $dir >/dev/null || continue
     echo THD plot
@@ -46,7 +54,7 @@ for ((i=0; i<length; i++)); do
     echo FreqResp plot
     python3 $cwd/audio_analyser_plot.py --autodetect --quiet --outfile "freqresp_${dir//\//_}.png" --title "${title} Frequency Response" --type "freqresp" --logx $arg
     echo Input Impedance plot
-    python3 $cwd/input_impedance_analogdiscovery.py --autodetect --quiet --outfile "zi_${dir//\//_}.png" --title "${title} Input Impedance" --logx
+    python3 $cwd/input_impedance_analogdiscovery.py --autodetect --quiet --outfile "zi_${dir//\//_}.png" --title "${title} Input Impedance" --logx $zi_arg
     echo Output Impedance plot
     python3 $cwd/output_impedance_analogdiscovery.py --autodetect --quiet --outfile "zo_${dir//\//_}.png" --title "${title} Output Impedance" --logx
     popd >/dev/null
