31b35887638ef845d2144568e394efa912c7b8a4
JohanASB <jaskja22@student.aau.dk>
2024-05-22 12:41:16 +0200

    revise normalized collector fig

--- figures/system_design/normalized_collector_fig_script.m
+++ figures/system_design/normalized_collector_fig_script.m
@@ -19,7 +19,10 @@ hold on
 plot([-0.8 -0.8],get(gca, 'ylim'),'k--','LineWidth',1)
 hold on
 plot([-10 0],[0.5 0.5],'--k','LineWidth',1);
-xlabel('Normalized differential input voltage','FontSize',14);
-ylabel('Normalized collector current','FontSize',14);
+
+xlabel('Normalized differential input voltage, $\frac{v_{id}}{V_T}$', 'Interpreter', 'latex','FontSize',14);
+ylabel('Normalized collector current, $\frac{i_{C}}{I}$', 'Interpreter','latex','FontSize',14);
 set(gca,'FontSize',16);
+text(6,0.92,'$\frac{i_{C1}}{I}$','Interpreter','latex','FontSize',20)
+text(-6,0.92,'$\frac{i_{C2}}{I}$','Interpreter','latex','FontSize',20)
 hold on
